b868af2a563e53752d54b732465ce02f7707a49d
yadavarnav10 <yadavarnav389@gmail.com>
2026-08-14 23:03:44 +0530

    Addition of user input for mass of black hole (solar masses) and Distance (meters)

--- Black_Hole.java
+++ Black_Hole.java
@@ -3,8 +3,6 @@ public class Black_Hole {
     public static final double c = 299792458; // in m/s
     public static final double SOLAR_MASS = 1.989e30; // in kg
     private double mass; // in kg
-    public static final double GEO_G = 1.0; // in geometric units
-    public static final double GEO_c = 1.0; // in geometric units
     public static final double conversion_factor = G / (c * c); // to convert mass to geometric units
     public static final double GEO_Solar_Mass = conversion_factor * SOLAR_MASS; // in geometric units
 
--- Main.java
+++ Main.java
@@ -1,12 +1,24 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Black_Hole blackHole = new Black_Hole(10); // 10 solar masses
+        System.out.println("Enter mass of Black Hole (Solar Masses):");
+        Scanner sc = new Scanner(System.in);
+        double mass = sc.nextDouble();
+        System.out.println("Enter distance from center of Black Hole (meters):");
+        double distance = sc.nextDouble();  
+        if (mass <= 0|| distance <= 0) {
+            System.out.println("INVALID INPUT. Mass and distance must be positive numbers.");
+            sc.close();
+            return;
+        }
+        Black_Hole blackHole = new Black_Hole(mass);
         double schwarzschildRadius = blackHole.getSchwarzschildRadius();
-        double gravitationalAcceleration = blackHole.gravitationalAcceleration(1e7); // at 10 million meters
-        double escapeVelocity = blackHole.escapeVelocity(1e7); // at 10 million meters
+        double gravitationalAcceleration = blackHole.gravitationalAcceleration(distance);
+        double escapeVelocity = blackHole.escapeVelocity(distance);
 
         System.out.println("Schwarzschild Radius: " + schwarzschildRadius + " meters");
         System.out.println("Gravitational Acceleration: " + gravitationalAcceleration + " m/s^2");
         System.out.println("Escape Velocity: " + escapeVelocity + " m/s");
+        sc.close();
     }
 }
